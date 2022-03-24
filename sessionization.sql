408f561a6be2e19d791343cce7e9daea5345b13e
jmainwaring <31424672+jmainwaring@users.noreply.github.com>
2022-03-23 22:30:56 -0400

    Added logic to group events by session

--- sessionization.sql
+++ sessionization.sql
@@ -23,12 +23,29 @@ time_diff AS (
 
 flurry_start_end AS ( 
     SELECT 
-        ts
+          user_id  
+        , ts
         , CASE WHEN (time_since_prev_event is NULL OR time_since_prev_event > 10) THEN 1 ELSE 0 END AS is_flurry_start
         , CASE WHEN (time_until_next_event is NULL OR time_until_next_event > 10) THEN 1 ELSE 0 END AS is_flurry_end
     FROM time_diff
+),
+
+session_id_creation AS (
+    SELECT
+          user_id
+        , ts
+        , CASE WHEN is_flurry_start THEN ROW_NUMBER() OVER (ORDER BY ts) ELSE NULL END AS initial_session_id
+    FROM flurry_start_end
+),
+
+full_session_id_assignment AS (
+    SELECT
+          user_id
+        , ts
+        , CASE WHEN is_flurry_start THEN initial_session_id
+               ELSE LAG(initial_session_id) IGNORE NULLS OVER (ORDER BY user_id, ts) END AS session_id
+    FROM session_id_creation
 )
 
-SELECT
-    ?
-FROM flurry_start_end
+SELECT *
+FROM full_session_id_assignment;
